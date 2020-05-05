e935192296a183e6abf5fc7005f3d2e2c23960b9
Zone Cảnh <canhzone861998@gmail.com>
2020-05-05 19:21:42 +0700

    Add bài 13, 14, 15

---
+++ b13-bt1-middleware/routes/user.route.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var router = express.Router();
+
+var controller = require('../controllers/user.controller');
+var validate = require('../validates/user.validate');
+
+router.get("/", controller.index);
+
+router.get('/search', controller.search);
+
+router.get('/create', controller.create);
+
+router.get('/:id/view', controller.view);
+
+router.get('/:id/delete', controller.delete);
+
+router.get('/:id/update', controller.update);
+
+router.post('/:id/update', controller.postUpdate);
+
+router.post('/create', validate.postCreate, controller.postCreate);
+
+module.exports = router;
